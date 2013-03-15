29265919020ed48fab67128f8336cd5e1eb31c48
Edouard Labre <edouard.labre@gmail.com>
2013-03-15 22:32:45 +0100

    Revert "Revert "some minor change for release""
    This reverts commit 6c98ad46b4d953241eb84627d2a2208edac785ef.

---
+++ minimal-blank-theme/searchform.php
@@ -0,0 +1,5 @@
+<form method="get" id="searchform" action="<?php echo esc_url( home_url() ); ?>" role="search">
+	<label for="s" class="label"><?php _e( 'Search', 'minimal-blank-theme' ); ?></label>
+	<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'minimal-blank-theme' ); ?>" />
+	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'minimal-blank-theme' ); ?>" />
+</form>
