3ce8933defb62806aa19269d389560cf51ddaa06
Edouard Labre <edouard.labre@gmail.com>
2013-03-15 22:37:43 +0100

    delete deplicated elements

--- minimal-blank-theme/sidebar.php
+++
@@ -1,5 +0,0 @@
-<?php if ( is_active_sidebar( 'Main Sidebar' ) ) : ?>
-	<section id="secondary" class="widget-area" role="complementary">
-		<?php dynamic_sidebar( 'Main Sidebar' ); ?>
-	</section><!-- #secondary -->
-<?php endif; ?>
