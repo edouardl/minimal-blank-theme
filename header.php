f44d5af1cd1e93f97d4e943926cb0bbf2d35dfbc
Edouard Labre <contact@edouardlabre.com>
2016-05-06 11:35:13 +0200

    Update query
    Update jQuery and jQuery-UI to be compatible with Bootstrap 3.3.6

--- header.php
+++ header.php
@@ -6,16 +6,16 @@
 <!--[if (gt IE 9)|!(IE)]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 
 	<title><?php wp_title(); ?></title>
-	
+
 	<meta name="author" content="<?php the_author_meta( 'display_name', 1 ); ?>">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
-	
+
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon.png" />
 	<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
-	
+
 	<?php
 	// Use jquery and jquery core from the google cdn instead of wordpress included
 	wp_deregister_script( 'jquery-ui-core' );
@@ -38,41 +38,41 @@
 	wp_deregister_script( 'jquery-ui-tabs' );
 	wp_deregister_script( 'jquery-ui-widget' );
 	wp_deregister_script( 'jquery' );
-	
-	wp_enqueue_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js', array(), '1.8.3' );
-	wp_enqueue_script( 'jquery-ui-core', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js', array( 'jquery' ), '1.9.2', true);
-	
+
+	wp_enqueue_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js', array(), '1.11.3' );
+	wp_enqueue_script( 'jquery-ui-core', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js', array( 'jquery' ), '1.11.4', true);
+
 	// Comment reply JS
 	if( is_singular() ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-	
-		
+
+
 	// @TODO : Uncomment what you need
-	
+
 	// Modernizr for html5 and CSS3 support
 	//wp_enqueue_script( 'modernizr', 'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js' , array(), '2.8.3', true);
-	
+
 	// Normalize
 	//wp_enqueue_style( 'normalize', 'https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css' );
-    
+
 	// Bootstrap
 	//wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js' , array('jquery'), '3.3.6', true);
 	//wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' );
-	
+
 	// ADD your css below
-		
-    
+
+
 	// ADD your js below
-	
-    
+
+
 	// Default theme stylesheet
 	wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/style.css' );
-    
+
 	// Default js of your theme to add your own js scripts, add dependances if needed
 	wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/js/scripts.js' , array( 'jquery' ), '1.0', true);
 
-		
+
     wp_head();
 	?>
 </head>
